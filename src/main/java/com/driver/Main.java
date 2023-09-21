cc05a881a57e5c0d5bc986f3a7552974f6826747
siva sankar <sivas2654@gmail.com>
2023-09-21 21:14:12 +0530

    first try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,31 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Product p = new Product();
+       int ans= p.product(2,3);
+        int ans1=p.product(2,3,4);
+        double ans2=p.product(2.0,5.0);
+        System.out.println(ans+" "+ans1+" "+ans2);
+
+    }
+
+
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
+
+
 }
